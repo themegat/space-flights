14b29ae030c6e9b72e8391ff04bfd7db47d96966
themegat <themegat@gmail.com>
2022-08-03 21:35:10 +0200

    Added some queries

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,13 +3,72 @@ const Queries = require("./persistance.js");
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
-    planets: () => Queries.getPlanets(),
-    async planet(parent, args, context, info) {
-      const data = await Queries.getPlanets();
-      return data.find((item) => item.code === args.code);
+    // hello: () => "Hello world!",
+    // async planet(parent, args, context, info) {
+    //   const data = await this.planets();
+    //   return data.find((item) => item.code === args.code);
+    // },
+
+    async planets(parent, args, context, info) {
+      const planets = await Queries.getPlanets();
+      const limit = args.limit || 5;
+      const spaceCenters = await Queries.getSpaceCenters();
+      return planets.map((planet) => {
+        return {
+          ...planet,
+          spaceCenters: spaceCenters
+            .filter((item) => item.planet_code == planet.code)
+            .splice(0, limit),
+        };
+      });
     },
-    spaceCenters: () => Queries.getSpaceCenters(),
+
+    async spaceCenters(parent, args, context, info) {
+      const pageSize = args.pageSize || 10;
+      const page = args.page || 1;
+      const offset = (page - 1) * pageSize;
+
+      let data = await Queries.getSpaceCenters();
+      const planets = await Queries.getPlanets();
+
+      data =
+        args.code === undefined
+          ? data
+          : data.filter((item) => item.planet_code === args.code);
+
+      const node = data
+        .slice(offset)
+        .slice(0, pageSize)
+        .map((item) => {
+          return {
+            ...item,
+            planet: planets.find((planet) => planet.code === item.planet_code),
+          };
+        });
+
+      return node;
+    },
+
+    async spaceCenter(parent, args, context, info) {
+      const id = args.id;
+      const uid = args.uid;
+
+      const data = await Queries.getSpaceCenters();
+      const planets = await Queries.getPlanets();
+
+      const node = data
+        .map((center) => {
+          return {
+            ...center,
+            planet: planets.find(
+              (planet) => planet.code === center.planet_code
+            ),
+          };
+        })
+        .find((center) => center.id === id || center.uid === uid);
+
+      return node;
+    },
   },
 };
 
--- src/schema.js
+++ src/schema.js
@@ -1,10 +1,17 @@
 const { gql } = require("apollo-server-koa");
 
 const typeDefs = gql`
+  type Pagination {
+    total: Int
+    page: Int
+    pageSize: Int
+  }
+
   type Planet {
     id: Int
     name: String
     code: String
+    spaceCenters: [SpaceCenter]
   }
 
   type SpaceCenter {
@@ -15,14 +22,23 @@ const typeDefs = gql`
     latitude: String
     longitude: String
     planet_code: String
+    planet: Planet
+  }
+
+  type PgSpaceCenter {
+    pagination: Pagination
   }
 
   type Query {
-    hello: String
-    planets: [Planet]
-    planet(code: String!): Planet
-    spaceCenters: [SpaceCenter]
+    planets(limit: Int!): [Planet]
+    spaceCenters(page: Int!, pageSize: Int!, code: String): [SpaceCenter]
+    spaceCenter(id: Int, uid: String): SpaceCenter
   }
 `;
 
 module.exports = typeDefs;
+
+/*
+   hello: String
+    planet(code: String!): Planet
+*/
